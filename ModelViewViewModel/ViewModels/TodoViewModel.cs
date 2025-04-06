41bbf3863e28b6463a9d70b5aab725e2d6f73624
Hemanth <trhemanth2@gmail.com>
2025-04-06 21:35:12 +0530

    update: added list

--- ModelViewViewModel/ViewModels/TodoViewModel.cs
+++ ModelViewViewModel/ViewModels/TodoViewModel.cs
@@ -1,24 +1,37 @@
 ﻿using ModelViewViewModel.Models;
 using ModelViewViewModel.Services;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 
 namespace ModelViewViewModel.ViewModels
 {
     public class TodoViewModel
     {
-        public Todo Todo { get; set; }
+        public string TodoItem { get; set; }
+
         private TodoService _todoService;
+
+        public ObservableCollection<Todo> TodoList {  get; set; }
+
         public ICommand AddCommand { get; set; }
+
+        public ICommand DeleteCommand { get; set; }
 
         public TodoViewModel()
         {
             _todoService = new TodoService();
-            AddCommand = new RelayCommand((todo) => this.AddTodo((Todo)todo), (p) => true);
+            AddCommand = new RelayCommand(this.AddTodo, (p) => true);
+            DeleteCommand = new RelayCommand(this.AddTodo, (p) => true);
+
+            TodoItem = "hemanth";
+            TodoList = [new Todo { Title = "Learn react" }];
         }
 
-        private void AddTodo(Todo todo) 
+        private void AddTodo(object? parameter) 
         {
+            var todo = new Todo { Title = this.TodoItem };
             _todoService.AddTodo(todo);
+            TodoList.Add(todo);
         }
     }
 }
